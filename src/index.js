25ab527eebdf4327017f22e6e588b16a140a5c5f
tvhfrks12678 <tvhfrks12678@gmail.com>
2021-04-20 12:34:32 +0900

    add Comment

--- src/index.js
+++ src/index.js
@@ -20,6 +20,7 @@ const user = {
   lastName: 'Perez',
   avatarUrl:
     'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_92x30dp.png',
+  name: 'Tarou',
 };
 
 const elementCreateElement = React.createElement(
@@ -39,26 +40,76 @@ const element = (
   </div>
 );
 
-// ReactDOM.render(
-//   elementCreateElement,
-//   // element,
-//   // <React.StrictMode>
-//   //   <App />
-//   // </React.StrictMode>,
-//   document.getElementById('root')
-// );
-
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  ReactDOM.render(element, document.getElementById('root'));
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
 }
 
-setInterval(tick, 1000);
+function AppWelcome() {
+  return (
+    <div>
+      <Welcome name="Sara" />
+      <Welcome name="Cahal" />
+      <Welcome name="Edite" />
+    </div>
+  );
+}
+
+const elementWelcome = <Welcome name="Sara" />;
+
+function formatDate(date) {
+  return date.toLocaleDateString();
+}
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <div className="UserInfo">
+        <img
+          className="Avatar"
+          src={props.author.avatarUrl}
+          alt={props.author.name}
+        />
+        <div className="UserInfo-name">{props.author.name}</div>
+      </div>
+      <div classNmae="Comment-text">{props.text}</div>
+      <div className="Comment-date">{formatDate(props.date)}</div>
+    </div>
+  );
+}
+
+const comment = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: 'https://placekitten.com/g/64/64',
+  },
+};
+
+ReactDOM.render(
+  <Comment date={comment.date} text={comment.text} author={comment.author} />,
+  // <AppWelcome />,
+  // elementWelcome,
+  // Welcome(user),
+  //elementCreateElement,
+  // element,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// function tick() {
+//   const element = (
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//     </div>
+//   );
+//   ReactDOM.render(element, document.getElementById('root'));
+// }
+
+// setInterval(tick, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
