f78825193c4168e6a2485d733999db78e0fa66e4
tvhfrks12678 <tvhfrks12678@gmail.com>
2021-04-20 12:52:44 +0900

    コンポーネントと props

--- src/index.js
+++ src/index.js
@@ -56,6 +56,14 @@ function AppWelcome() {
 
 const elementWelcome = <Welcome name="Sara" />;
 
+function sum(a, b) {
+  return a + b;
+}
+
+function withdraw(account, amount) {
+  account.total -= amount;
+}
+
 function formatDate(date) {
   return date.toLocaleDateString();
 }
@@ -63,17 +71,25 @@ function formatDate(date) {
 function Comment(props) {
   return (
     <div className="Comment">
-      <div className="UserInfo">
-        <img
-          className="Avatar"
-          src={props.author.avatarUrl}
-          alt={props.author.name}
-        />
-        <div className="UserInfo-name">{props.author.name}</div>
-      </div>
+      <UserInfo user={props.author} />
       <div classNmae="Comment-text">{props.text}</div>
       <div className="Comment-date">{formatDate(props.date)}</div>
     </div>
+  );
+}
+
+function UserInfo(props) {
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.user} />
+      <div className="UserInfo-name">{props.user.name}</div>
+    </div>
+  );
+}
+
+function Avatar(props) {
+  return (
+    <img className="Avatar" src={props.user.avatarUrl} alt={props.user.name} />
   );
 }
 
