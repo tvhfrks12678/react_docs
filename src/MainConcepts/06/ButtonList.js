c1a250c5b4de02e130735696289ee8817c91f869
tvhfrks12678 <tvhfrks12678@gmail.com>
2021-04-29 14:38:11 +0900

    Test

--- src/MainConcepts/06/ButtonList.js
+++ src/MainConcepts/06/ButtonList.js
@@ -1,10 +1,29 @@
 import React from 'react';
 
 class ButtonList extends React.Component {
-  handleClick(item, e) {
+  handleClickEvent(item, e) {
     console.log('item is:', item);
     console.log('event is:', e);
+    console.log('this is:', this);
   }
+
+  handleClick = (word, item, index) => {
+    // e.preventDefault();
+
+    console.log('word is:', word);
+    console.log('item is:', item);
+    console.log('index is:', index);
+    console.log('this is:', this);
+  };
+
+  handleClickTest = (word, item) => {
+    // e.preventDefault();
+
+    console.log('word is:', word);
+    console.log('item is:', item);
+    // console.log('index is:', index);
+    // console.log('this is:', this);
+  };
 
   render() {
     const word = ['one', 'two', 'three'];
@@ -12,7 +31,15 @@ class ButtonList extends React.Component {
       <div>
         {word.map((item, index) => (
           <li key={index}>
-            <button onClick={(e) => this.handleClick(item, e)}>Click me</button>
+            <button onClick={(e) => this.handleClickEvent(item, e)}>
+              Click me Event
+            </button>
+            <button onClick={this.handleClick.bind(this, item, index)}>
+              Click me
+            </button>
+            <button onClick={this.handleClickTest.bind(item)}>
+              Click me Test
+            </button>
           </li>
         ))}
       </div>
