8ea0e15addde86732ec0a8e82496db0bb07047de
tvhfrks12678 <tvhfrks12678@gmail.com>
2021-05-09 16:37:15 +0900

    Correct Key Example

--- src/MainConcepts/08/NumberList.js
+++ src/MainConcepts/08/NumberList.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+function ListItem(props) {
+  return <li>{props.value}</li>;
+}
+
 function NumberList(props) {
   const numbers = props.numbers;
   const listItems = numbers.map((number) => (
-    <li key={number.toString()}>{number}</li>
+    <ListItem key={number.toString()} value={number} />
   ));
   return <ul>{listItems}</ul>;
 }
